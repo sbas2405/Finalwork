3f0aba0130279101a7df000c4ff2f6d0f5d4df62
sbas2405 <juans.gonzalezc@ecci.edu.co>
2023-11-22 20:19:05 -0500

    .

--- R/Finalwork.R
+++ R/Finalwork.R
@@ -31,7 +31,7 @@ Data_PI<- Data_PPI %>%
   arrange(desc(total_mode))
 print(Data_PI)
 PPI_INIC_FINAL<-colnames(final_data_filtrado)[ 8:ncol(final_data_filtrado)]
-aux2 <- AMCBGeneUtils::changeGeneId(PPI_INIC_FINAL, from = "Ensembl.ID")
+aux2<- AMCBGeneUtils::changeGeneId(PPI_INIC_FINAL, from = "Ensembl.ID")
 names(final_data_filtrado)[8:11357] <- aux2$HGNC.symbol
 FINAL_GENES<- colnames(final_data_filtrado)
 Data_PI_Filtrado<- Data_PI%>%
@@ -39,7 +39,27 @@ filter(gen %in% FINAL_GENES)
 #Step 5
 #PART A
 #KNN MODEL
-
-
-
-
+set.seed(18)
+Predictores <- as.vector(head(Data_PI_Filtrado[, 1], 100))
+Predictores <- as.character(unlist(Predictores))
+colnames(final_data_filtrado)[is.na(colnames(final_data_filtrado))] <- paste0("xs", seq_along(colnames(final_data_filtrado)[is.na(colnames(final_data_filtrado))]))
+final_data_filtrado_PRD <- final_data_filtrado %>%
+  group_by(sample_type) %>%
+  sample_n(123, replace = TRUE) %>%
+  ungroup()
+sample.index <- sample(10:nrow(final_data_filtrado_PRD), nrow(final_data_filtrado_PRD) * 0.75, replace = FALSE)
+train.data <- final_data_filtrado_PRD[sample.index, c(Predictores, "sample_type"), drop = FALSE]
+test.data <- final_data_filtrado_PRD[-sample.index, c(Predictores, "sample_type"), drop = FALSE]
+train.data$sample_type <- factor(train.data$sample_type)
+test.data$sample_type <- factor(test.data$sample_type)
+control <- trainControl(method = "cv", p = 0.75)
+knnFit <- train(sample_type ~ .,
+                data = train.data,
+                method = "knn",
+                trControl = control,
+                preProcess = c("range"),
+                tuneLength = 50)
+print(knnFit)
+plot(knnFit)
+knnPredict <- predict(knnFit, newdata = test.data)
+confusionMatrix(data = knnPredict, reference = test.data$sample_type)
